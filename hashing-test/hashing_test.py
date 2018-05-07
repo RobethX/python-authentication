be4f96fd94f36380e335b695768ad3c99063471e
Rob Chiocchio <r.mchiocchio@gmail.com>
2018-05-07 08:39:34 -0400

    Added comment reminders

--- hashing-test/hashing_test.py
+++ hashing-test/hashing_test.py
@@ -18,6 +18,7 @@ else:
 c = db.cursor()
 
 username = input("Username: ")
+#filter username for letters and numbers only (prevent sql injection)
 password = base64.b64encode((getpass.getpass("Password: ") + "NaCl").encode()) #b64 to prevent it from ever being in plain (readable) text - is this useless?
 hash = bcrypt.hashpw(password, bcrypt.gensalt(12))
 
@@ -26,6 +27,7 @@ data = c.fetchone()
 if not data:
     if "y" in input('Account with username %s not found. Would you like to create a new account? [y/n]' %username).lower():
         email = input("Email address: ")
+        #filter email for letters, numbers, and accepted characters only (prevent sql injection)
         c.execute("INSERT INTO accounts values (?,?,?,?,?)", [username, hash, email, int(time.time()), int(time.time())]) #set date accessed and date created to current time
         print("Account created.")
 else:

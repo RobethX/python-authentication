54ff00e8c17d1dfe2779e70e41ec1ddb5c2e9c1e
Rob Chiocchio <RobethX@users.noreply.github.com>
2018-05-08 20:49:17 -0400

    removed variable "hash"

--- hashing-test/hashing_test.py
+++ hashing-test/hashing_test.py
@@ -20,7 +20,7 @@ c = db.cursor()
 username = input("Username: ")
 #filter username for letters and numbers only (prevent sql injection)
 password = base64.b64encode((getpass.getpass("Password: ") + "NaCl").encode()) #b64 to prevent it from ever being in plain (readable) text - is this useless?
-hash = bcrypt.hashpw(password, bcrypt.gensalt(12))
+passwordHash = bcrypt.hashpw(password, bcrypt.gensalt(12))
 
 c.execute("SELECT * FROM accounts WHERE username = ? COLLATE NOCASE", (username,))
 data = c.fetchone()
@@ -28,7 +28,7 @@ if not data:
     if "y" in input('Account with username %s not found. Would you like to create a new account? [y/n]' %username).lower():
         email = input("Email address: ")
         #filter email for letters, numbers, and accepted characters only (prevent sql injection)
-        c.execute("INSERT INTO accounts values (?,?,?,?,?)", [username, hash, email, int(time.time()), int(time.time())]) #set date accessed and date created to current time
+        c.execute("INSERT INTO accounts values (?,?,?,?,?)", [username, passwordHash, email, int(time.time()), int(time.time())]) #set date accessed and date created to current time
         print("Account created.")
 else:
     if bcrypt.checkpw(password, data[1]):
